e473f59b07f9b0e5b3bb61d87282b48023d987a1
DNI9 <itsr9boi@gmail.com>
2022-04-26 21:57:06 +0530

    chore: replace react with preact in client production build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
@@ -15,4 +18,18 @@ module.exports = withBundleAnalyzer({
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
   swcMinify: true,
+
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 });
